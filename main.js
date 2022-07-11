c13a805bb48a8c87b8b342e9d15ae7ce7d9cf313
ZhekaLited <zhenyagorovtsov@gmail.com>
2022-07-11 19:51:09 +0300

    4

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const slider = document.querySelector(".slider");
 const sliderLine = document.querySelector(".slidr__line");
 const images = document.querySelectorAll(".slide img");
+const prevBtn = document.querySelector("slider__prevBtn");
+const nextBtn = document.querySelector("slider__nextBtn");
 
-
-let orderImg = 0
-let width
-
+let orderImg = 0;
+let width;
 
 function initWidthLine() {
-    width = slider.offsetWidth
-    console.log(width);
+  width = slider.offsetWidth;
+  sliderLine.style.width = width * images.length + "px";
+  images.forEach((img) => {
+    img.style.width = width + "px";
+    img.style.height = height + "px";
+  });
 }
 
-initWidthLine()
+window.addEventListener("resize", initWidthLine);
+
+initWidthLine();
+
+nextBtn.addEventListener("click", function () {
+  orderImg++;
+  scrollSlider();
+});
+
+function scrollSlider() {
+  sliderLine.style.transform = "translate(- " + orderImg * width + "px)";
+}
